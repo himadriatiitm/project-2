58991330f555ce8bf989a06feebd54f53f1b1bfe
Himadri Bhattacharjee <24f3004990@ds.study.iitm.ac.in>
2025-08-10 15:22:57 +0530

    very very bad code

--- main.py
+++ main.py
@@ -1,42 +1,96 @@
+import sys
+from pprint import pprint
+import traceback
+import sandbox
 from fastapi import FastAPI, UploadFile, HTTPException, Response
 from typing import List, Any, Dict
 import llm
 import os
 import pandas as pd
 import numpy as np
-from func_timeout import func_set_timeout,FunctionTimedOut
+from func_timeout import func_set_timeout, FunctionTimedOut
 import ssl
+from utils import extract_code
+import re
 import duckdb
 from pathlib import Path
-import re
+import json
+import tools
+import io
+import inspect
+import contextlib
+from iochain import IOBlock, IOChain
+from dataclasses import dataclass
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
+
 def format_ns(ns) -> str:
-    return ' '.join(list(ns))
+    return " ".join(list(ns))
 
-GOG = ""
+# AVAILABLE_FUNCTIONS = ""
 
-def printer(*args):
-    global GOG
-    GOG += ' '.join(map(str, args))
+# for fn in tools.__all__:
+#     doc = inspect.getdoc(fn)
+#     sig = str(inspect.signature(fn))
+#     name = fn.__name__
+#     AVAILABLE_FUNCTIONS += f"""
+# tools.{name}{sig}
+# {doc}
+# """
+
+# AVAILABLE_FUNCTIONS = f"""
+# <available-functions>
+# {AVAILABLE_FUNCTIONS}
+# </available-functions>
+# """
+
+# print(AVAILABLE_FUNCTIONS)
+
+AGENT_SEED = (
+    "You are a senior Python developer with a lot of experience in data science frameworks.\n"
+    "You use succinct code that gets the job done without relying too much on extraneous libraries.\n"
+    "Never write comments. There is only one correct way to solve a given problem. - The Zen of Python\n\n"
+)
 
 @func_set_timeout(20)
-def evaluate_code(code, local_namespace):
-    global GOG
-    GOG = ""
-    exec(code, {'pd': pd, 'np': np, 'ssl': ssl, 'duckdb': duckdb, 'print': printer}, local_namespace)
+def evaluate_code(code, ns):
+    stdout_buffer = io.StringIO()
+    stderr_buffer = io.StringIO()
 
-    # Extract the result (assuming it's the last evaluated expression)
-    if GOG:
-        return GOG
+    with (
+        contextlib.redirect_stdout(stdout_buffer),
+        contextlib.redirect_stderr(stderr_buffer),
+    ):
+        sandbox.exec(
+            code,
+            {
+                "pd": pd,
+                "np": np,
+                "ssl": ssl,
+                "duckdb": duckdb,
+                "tools":tools,
+            },
+            ns,
+        )
 
-    result_id = list(local_namespace)[-1]
-    return local_namespace[result_id]
+    stderr = stderr_buffer.getvalue()
+    if stderr and "and thus cannot be shown" not in stderr:
+        stdout = stdout_buffer.getvalue()
+        # Extract the result (assuming it's the last evaluated expression)
+        raise ValueError("WARNING: " + stderr + "\n\n" + stdout)
+
+    if stdout := stdout_buffer.getvalue():
+        return stdout
+
+    if result_id := next(iter(ns)):
+        return ns[result_id]
+
 
 app = FastAPI()
-model = llm.get_model("gpt-4o-mini")
+model = llm.get_model("gpt-4o")
 model.key = os.environ["OPENAI_API_KEY"]
+
 
 @app.post("/api/")
 async def upload_file(file: List[UploadFile]):
@@ -47,7 +101,7 @@ async def upload_file(file: List[UploadFile]):
     try:
         for handle in file:
             filename = Path(handle.filename).name
-            if filename == 'question.txt':
+            if filename == "question.txt":
                 question = (await handle.read()).decode()
                 continue
             Path(filename).write_bytes(await handle.read())
@@ -55,8 +109,8 @@ async def upload_file(file: List[UploadFile]):
         raise HTTPException(status_code=500, detail=f"error reading file: {str(e)}")
 
     if not question:
-        return {"message":"that's a stupid request."}
-    
+        return {"message": "that's a stupid request."}
+
     tries = 5
     answer = None
     for truh in range(tries):
@@ -66,180 +120,177 @@ async def upload_file(file: List[UploadFile]):
         except FunctionTimedOut:
             continue
 
-    return Response(content=answer, media_type='application/json')
-    return answer
+        if not isinstance(json.loads(answer), List):
+            continue
 
-@func_set_timeout(180)
+    return Response(content=answer, media_type="application/json")
+
+
+
+@func_set_timeout(90)
 def answer_attempt(question):
-    local_namespace: Dict[str, Any] = {}
+    ns: Dict[str, Any] = {}
     # I swear I hate this god awful snake language.
     # Take me back to C.
-    code: str | None = None
-    last_known_good_code: str = ""
-    result: Any | Exception = None
-    steps = breakdown(question)
+    io_chain = IOChain([])
+    steps = must_breakdown(question)
+    print(steps)
+
     i = 0
-    while i < len(steps):
-        instr = steps[i]
-        ok = False
-        while not ok:
-            try:
-                if isinstance(result, Exception):
-                    code = rectify_code(instr, truncate_string(result), last_known_good_code, code)
-                else:
-                    code = broad_eval(question, instr, list(local_namespace), last_known_good_code, truncate_string(result))
-            except Exception as e:
-                print(e)
-                continue
-            print("== LLM says " + '=' * 68)
-            print(code)
-            print('=' * 80)
-            result = None
-            try:
-                result = evaluate_code(code, local_namespace)
-            except FunctionTimedOut:
-                result = "<timeout />"
-            except Exception as e:
-                result = e
-                continue
+    for i, step in enumerate(steps):
+        try:
+            code = llm_generate(step, io_chain)
+        except Exception as e:
+            print("answer_attempt::warning ", e)
+            continue
+        print(f"<LLM>\n{code}\n</LLM>")
+        try:
+            block = IOBlock(code, evaluate_code(code, ns))
+            io_chain.chain.append(block)
+        except Exception as e:
+            print("answer_attempt::exception ", e)
+            ns = rectify_loop(step, io_chain, code, e)
 
-            if wanna_backtrack(result, local_namespace, code):
-                # backtrack
-                i -= 2
+        if len(io_chain.chain) > 0:
+            print(io_chain.chain[-1].result)
+            print(format_ns(ns))
 
-            ok = True
-            last_known_good_code += "\n" + code
+    return io_chain.chain[-1].result
 
-            if match := re.match(r'(\w+)\s*=\s*json.dumps', code):
-                result_id = match.group(1)
-                return local_namespace[result_id]
 
-            print(truncate_string(str(result)))
-            print(format_ns(local_namespace))
-        i += 1
-
-    return result
-    
-
-def between_rope(string: str, rope_start: str, rope_end: str = None) -> str:
-    rope_end = rope_end or rope_start
-    start = string.index(rope_start) + len(rope_start)
-    string = string[start:]
-    end = string.index(rope_end)
-    return string[:end]
-    
-
-def between_tags(string: str, rope: str) -> str:
-    rope_start = f"<{rope}>"
-    rope_end = f"</{rope}>"
-    return between_rope(string, rope_start, rope_end)
-
-def extract_code(code: str) -> str:
-    code = between_rope(code, "```")
-    if code.startswith("python"):
-        code = code[len("python"):]
-    return code
-
-def truncate_string(s: Any, length = 200) -> str:
-    s = str(s)
-    if len(s) > length:
-        return s[:length-4] + "..." + s[-4:]
-    return s
-
-def rectify_code(objective: str, result: str, prev_cell: str, last_code: str) -> str:
+def rectify_code(objective: str, io_chain: IOChain, erroneous_code: str, exc: Exception) -> str:
     system = (
-        "You are a senior Python developer with a lot of experience in data science frameworks.\n"
-        "You use succinct code that gets the job done without relying too much on extraneous libraries.\n"
-        "You do not write comments. Comments are for the weak. There is only one correct way to solve a given problem. - The Zen of Python\n\n"
-        "Rectify your previous code according to the errors. Each step is exploratory so you decide to print and examine the outputs when needed."
+        AGENT_SEED + "Rectify your previous code according to the errors."
     )
-    prompt = f"""
+    prompt = fr"""
+
 Code that executed successfully so far:
 ```python
-{prev_cell}
+{io_chain}
 ```
 
-Code:
+Erroneous code:
 ```python
-# {objective}
-{last_code}
+{erroneous_code}
 ```
 
-Error:
+```error
+{exc}
 ```
-{result}
-```
+
+Write <= 6 lines of Python to rectify your previous code.
+If some information like a column name is ambiguous, try to find that out in this cell.
+You will be given another chance to put it all together. Just add a trailing comment "# partial step".
+
 """
+    # + AVAILABLE_FUNCTIONS
     response = model.prompt(prompt, system=system).text()
-    return extract_code(response)
+    code = extract_code(response)
+    if code.count("\n") > 6:
+        raise ValueError("LLM produced slop")
+    return code
 
-def wanna_backtrack(result: Any, ns: List[str], last_code) -> str:
+# @param inout ic_chain
+def rectify_loop(objective: str, io_chain: IOChain, erroneous_code: str, exc: Exception) -> Dict[str, Any]:
+    while True:
+        ns = {}
+        try:
+            code = rectify_code(objective, io_chain, erroneous_code, exc)
+            code = f"""
+        # {objective}
+        {code}
+        """
+        except Exception as e:
+            print("while asking llm to rectify code: ", e)
+            continue
+        try:
+            result = evaluate_code(io_chain.dump_with(code), ns)
+        except FunctionTimedOut:
+            result = "<timeout />"
+        except Exception as e:
+            exc = e
+            objective = code
+            continue
+        io_chain.chain.append(IOBlock(code, result))
+        return ns
+
+def llm_generate(instr: str, io_chain: IOChain, full_question: str = "") -> str:
+    system = AGENT_SEED + "Implement the described function and call it.\n"
+    if io_chain:
+        system += f"""
+Code from the last {io_chain.limit} cells:
+{io_chain}
+"""
+
+    response = model.prompt(
+f"""
+Write <= 6 line of Python to perform the logic in this pseudocode statement.
+If some information like a column name is ambiguous, try to find that out in this cell.
+You will be given another chance to put it all together. Just add a trailing comment "# partial step".
+
+```python
+# {instr}
+""",
+        system=system,
+    ).text()
+    code = extract_code(response)
+    if code.count("\n") > 6:
+        raise ValueError("LLM produced slop")
+    code = f"""
+# {instr}
+{code}
+"""
+    return code
+
+@dataclass
+class Step:
+    identifier: str
+    function_call: str
+
+    def __init__(self, step: str):
+        match = re.match(r'(\w+)\s*=\s*(.*)', step)
+        self.identifier = match.group(1)
+        self.function_call = match.group(2)
+
+    def __repr__(self) -> str:
+        return f"{self.identifier} = {self.function_call}"
+
+def breakdown(question: str) -> List[Step]:
     system = """
-Make sure that the variables you created are actually present in the local namespace.
-Respond with
-- YES: to proceed to then ext step.
-- BACKTRACK: to go back to a previous step.
-"""
-    result = truncate_string(str(result), 200)
-    ns = format_ns(ns)
-    proompt = f"""
-Your code:
-```python
-{last_code}
-```
+Break the following question down into a sequence of function calls and assignments in pseudocode.
+Use links and filenames as verbatim. If an idea does not fit in a function call, break it down further.
 
-<output>
-{result}
-</output>
+<output-format>
+df = scrape_table("https://doc.e.foundation/devices")
+devices_col = find_column_containing(df, "devices")
+plot = perform_scatterplot(devices_col)
+</output-format>
 
-<locals>{ns}</locals>
-"""
-    response = model.prompt(proompt, system=system).text()
-    return "BACKTRACK" in response
-
-def broad_eval(question:str, instr: str, ns: List[str], last_code, result) -> str:
-    system = (
-        "You are a senior Python developer with a lot of experience in data science frameworks.\n"
-        "You use succinct code that gets the job done without relying too much on extraneous libraries.\n"
-        "You do not write comments. Comments are for the weak. You do not use magic numbers or magic literals.\n"
-        "You write and examine code one step at a time, starkly contrasing the haste of junior developers who try to solve a problem in one go.\n"
-        "There is only one correct way to solve a given problem. - The Zen of Python\n\n"
-        "Write Python code for the described step. Each step is exploratory so you decide to print and examine the outputs when needed."
-    )
-    if ns:
-        ns = format_ns(ns)
-        system += (
-            "These local variables exist from your previous run.\n"
-            f"<locals>{ns}</locals>"
-        )
-    if last_code:
-        system += f"""Code executed so far:
-```python
-{last_code}
-```
-
-output:
-```
-{result}
-```
-"""
-    response = model.prompt("```python\n# " + instr, system=system).text()
-    return extract_code(response)
-
-
-def breakdown(question: str) -> List[str]:
-    system = """Break the following question down into a list of steps to be taken in Python.
-Use links and filenames as verbatim. Describe each step in a single line.
-If the step does not fit in a line, break it down further. Answer only the list of steps so that our API can ingest them with ease.
-
-Example:
-
-1. Scrape the table from https://doc.e.foundation/devices
-2. Read the image `phone.png` into variable `img` with PIL
+Always follow the format `identifier = function_call(...)`
+No comments. No imports. No multiline expression.
 """
 
     response = model.prompt(question, system=system).text()
-    return response.splitlines()
+    print(response)
+    actions = []
+    for line in response.splitlines():
+        if not line:
+            continue
+        if line.startswith("```"):
+            continue
+        try:
+            step = Step(line)
+        except Exception as e: # noqa
+            print(e)
+            return None
+        actions.append(step)
+    return actions
+
+def must_breakdown(question: str) -> List[Step]:
+    steps = None
+    while steps is None:
+        steps = breakdown(question)
+    return steps
 
 if __name__ == "__main__":
     import uvicorn
