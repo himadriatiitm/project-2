5f8dff665499913535dc4f9ad414d0bc9b14e2d4
Himadri Bhattacharjee <24f3004990@ds.study.iitm.ac.in>
2025-08-13 18:04:08 +0530

    isolate modules

--- src/faux.py
+++ src/faux.py
@@ -1,29 +1,27 @@
 import logging
 import json
 from typing import List, Any
+from textwrap import dedent
 
-def _forge(question: str, model) -> List[Any] | None:
-    system = """
-Generate the most believable output. It will always be a JSON array.
+def forge(question: str, model) -> List[Any] | None:
+    system = dedent("""\
+        Generate the most believable output. It will always be a JSON array.
 
-```json
-[valueA, valueB, ...]
-```
+        Rules:
+        - Infer the datatype of each answer.
+        - How many (count) should be a Number.
+        - Percentages, correlations, and the like must be floating point `Number`s.
+        - For base64 images, output the dummy string "$plot".
 
-Rules:
-- Infer the datatype of each answer.
-- How many (count) should be a Number.
-- Percentages, correlations, and the like must be floating point `Number`s.
-- For base64 images, output `$plot` without any quotes.
-"""
+        FORMAT:
+
+        ```json
+        [valueA, valueB, ...]
+        ```
+        """)
     response = model.prompt(question, system=system).text()
     logging.info(response)
     return list_verify(json_verify(response))
-
-def forge(question: str, model) -> List[Any]:
-    while True:
-        if faux := _forge(question, model):
-            return faux
 
 def json_verify(response: str):
     try:
---
+++ src/main.py
@@ -0,0 +1,167 @@
+import sandbox
+from fastapi import FastAPI, UploadFile, HTTPException
+from typing import List, Any, Callable
+import llm
+import os
+import pandas as pd
+import numpy as np
+from func_timeout import func_set_timeout, FunctionTimedOut
+import ssl
+from utils import extract_code, s
+import duckdb
+from pathlib import Path
+import faux
+import agent
+import logging
+
+# top-level monkey-patching
+logging.basicConfig(level=logging.INFO)
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+def format_ns(ns) -> str:
+    return " ".join(list(ns))
+
+
+app = FastAPI()
+model = llm.get_model("gpt-4o")
+model.key = os.environ.get("OPENAI_API_KEY") or "electric-boogaloo"
+tries = int(os.environ.get("MAX_TRIES", 3))
+
+
+# TODO: keep a trace of all transactions
+@app.post("/api/")
+async def upload_file(file: List[UploadFile]):
+    """
+    Accepts a text file on the /api/ route and returns its content.
+    """
+    question = None
+    try:
+        for handle in file:
+            filename = Path(handle.filename).name
+            if filename == "question.txt":
+                question = (await handle.read()).decode()
+                continue
+            Path(filename).write_bytes(await handle.read())
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"error reading file: {str(e)}")
+
+    if not question:
+        return {"message": "next time, come up with a question I can answer."}
+
+    answer = must(faux.forge, question, model)
+    for _ in range(tries):
+        try:
+            answer = answer_attempt(question)
+            break
+        except FunctionTimedOut:
+            continue
+
+    return answer
+
+
+class Platypus:
+    def __init__(self, question: str):
+        self.generated = False
+        self.question = question
+        self.code = None
+        self.imports = {
+            "pd": pd,
+            "np": np,
+            "ssl": ssl,
+            "duckdb": duckdb,
+        }
+        self.ns = dict()
+        self.result = None
+
+    def __call__(self):
+        f = rectify if self.generated else generate
+        self.generated = True
+        self.code = must(f, self.question, self.code, self.result)
+        logging.info(self.code)
+        self.result = sandbox.exec(self.code, self.imports, self.ns)
+        logging.info(f"{self.result=}")
+        return last_line_json_list(self.result)
+
+
+@func_set_timeout(90)
+def answer_attempt(question):
+    return must(Platypus(question))
+
+
+def last_line_json_list(result: str) -> List[Any]:
+    lines = result.strip().splitlines()
+    if len(lines) == 0:
+        return None
+    line = lines[-1]
+    if any((badbad in line.lower() for badbad in ("nan", "not found"))):
+        return None
+    return faux.list_verify(faux.json_verify(line))
+
+
+def rectify(objective: str, code: str, exc: str) -> str:
+    system = agent.description + "Rectify your previous code according to the errors."
+    prompt = s("""
+        objective:
+        ```
+        {}
+        ```
+
+        ```python
+        {}
+        ```
+
+        ```output
+        {}
+        ```
+
+        Rectify the code in the last block.
+        In case of doubt, `print`. You will be given multiple attempts.
+        Always `print(json.dumps(...))` the final list. It may contain int, float and str.
+        """, objective, code, exc)
+
+    logging.info(
+        s(
+            """
+        TO LLM:
+
+        SYSTEM:
+        ```
+        {}
+        ```
+
+        PROMPT:
+
+        ```
+        {}
+        ```
+        """,
+            system,
+            prompt,
+        )
+    )
+
+    response = model.prompt(prompt, system=system).text()
+    return extract_code(response)
+
+
+def generate(question: str, *args) -> str:
+    system = agent.description + s("""\
+        Write python code to solve the following data science question.
+        Always `print(json.dumps(...))` the final list. It may contain int, float and str.
+        """)
+
+    response = model.prompt(question, system=system).text()
+    return extract_code(response)
+
+
+def must(f: Callable, *args, **kwargs):
+    while True:
+        if v := f(*args, **kwargs):
+            return v
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
